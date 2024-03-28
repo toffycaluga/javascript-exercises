9d5f880485052bf0acff1e68c434fd842c19e2d1
Toffy Caluga <abraham.lillol@gmail.com>
2024-03-28 14:41:02 -0300

    feat: enlace a charcounter listo

--- assets/js/charCounter.js
+++ assets/js/charCounter.js
@@ -1,1 +1,15 @@
-{/* char counter js */ }
+// char counter js
+function countCharacters() {
+
+
+    //seleccionamos el input
+    const input = document.getElementById("textInput");
+
+
+    //seleccionamos el charCount
+    const charCount = document.getElementById("charCount");
+
+
+    //cambiamos el contenido del span id=charCount por la cantidad de los caracteres del input
+    charCount.textContent = input.value.length;
+}
