30414ebae0cdea22035a55a66f35c937d28c37ce
Toffy Caluga <abraham.lillol@gmail.com>
2024-03-28 16:00:04 -0300

    feat: enlace a click counter hecho

--- assets/js/clickCounter.js
+++ assets/js/clickCounter.js
@@ -1,1 +1,19 @@
 // clickcounter js
+
+
+let contadorClicks = 0;
+
+// se hace una funcion para aumentar en el contador
+function aumentar() {
+    const clickCount = document.getElementById('clickCount');
+
+    // aumentamos en 1 el contador
+    contadorClicks++;
+
+    // Aqui hay 2 maneras de concatenar la variable y el string a utilizar
+
+    // clickCount.textContent = contadorClicks + " clicks";
+
+    clickCount.textContent = `${contadorClicks} clicks`;
+
+}
